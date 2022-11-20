6bab2e1cb8ac35a5f4364dcf89c98b435e8ee31f
Maya Mathur <mmathur@stanford.edu>
2022-11-20 09:48:25 -0800

    fixed_time_interval gen'lazations

--- examples.R
+++ examples.R
@@ -4,7 +4,13 @@ setwd(here)
 source("helper_workout.R")
 
 
-# swimming intervals: generate printable times to leave the wall
-# swim 10 sets, leaving the wall every 1:55
-swimterval_chart(10, "01:55")
+# INTERVAL EXAMPLES ---------------------------------------
 
+# generate fixed-time intervals for swimming
+# 10 evenly-paced sets, leaving the wall every 1:55
+fixed_time_intervals(10, "02:15")
+
+# fancier 2-wave ladder structure with 4th set easier
+fixed_time_intervals(8,
+                     leave_every_mm_ss = "02:15",
+                     target_work_times = rep( c("1:48", "1:47", "1:46", "1:55"), 2 ) )
--- helper_workout.R
+++ helper_workout.R
@@ -1,5 +1,56 @@
 
 
+# FNS FOR INTERVALS ---------------------------------------------
+
+### For fixed-time intervals (e.g., swim intervals where you leave the wall
+#  every 2:00), generates a chart of times to leave. Optionally generates target return times depending on how long each work set should take.
+
+# n_sets: number of intervals
+# leave_every_mm_ss: quoted string "mm:ss" for how long each interval, including rest, lasts
+# target_work_times: quoted string "mm:ss" for how the target work times for each interval. If length = 1, assumes each target time is the same (i.e. even pacing).
+fixed_time_intervals = function(n_sets,  
+                                leave_every_mm_ss,
+                                target_work_times = NULL ){
+  
+  # # TEST only
+  # n_sets = 4
+  # leave_every_mm_ss = "02:15"
+  # target_work_times = c("1:48", "1:47", "1:46", "1:55")
+  
+  require(lubridate)
+  
+  leave_every_sec = period_to_seconds( ms(leave_every_mm_ss) )
+
+  
+  # last time I should leave the wall
+  leave_time_sec = ( 0 : (n_sets-1) ) * leave_every_sec 
+  
+  d = data.frame( set = 1:n_sets,
+              leave_at = sec_to_mm_ss(leave_time_sec) )
+  
+  
+  
+  if ( !is.null(target_work_times) ) {
+    
+    if ( length(target_work_times) == 1 ){
+      target_work_times = rep( target_work_times, n_sets )
+    }
+    
+    if ( length(target_work_times) < n_sets ) {
+      stop("target_work_times should have length equal to n_set or 1")
+    }
+    
+    target_work_sec = period_to_seconds( ms(target_work_times) )
+   
+    d$return_at = sec_to_mm_ss(leave_time_sec + target_work_sec)
+  }
+  
+  return(d)
+  
+}
+
+
+# BASIC UTILITY FNS ---------------------------------------------
 # convert sec to string "mm:ss"  
 sec_to_mm_ss = function(t){
   paste(paste(formatC(t %/% 60 %% 60, width = 2, format = "d", flag = "0"),
@@ -10,23 +61,4 @@ sec_to_mm_ss = function(t){
 }
 # example
 # sec_to_mm_ss(1305)
-
-
-# swim interval chart
-swimterval_chart = function(n_sets,  # number of intervals
-                            leave_every_mm_ss  # quoted string "mm:ss" for how long each interval, including rest, lasts
-                            ){
-  
-  require(lubridate)
-  
-  leave_every_sec = period_to_seconds( ms(leave_every_mm_ss) )
-  
-  # last time I should leave the wall
-  leave_time_sec = ( 0 : (n_sets-1) ) * leave_every_sec 
-  
-  
-  data.frame( set = 1:n_sets,
-                  leave_at = sec_to_mm_ss(leave_time_sec) )
-
-}
 
