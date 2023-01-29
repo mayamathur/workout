4d036b4fec5aecc1dde9c4858bd0ddeabe1cdc1c
Maya Mathur <mmathur@stanford.edu>
2023-01-29 08:05:02 -0800

    add fn pace_to_run_miles

--- examples.R
+++ examples.R
@@ -4,11 +4,28 @@ setwd(here)
 source("helper_workout.R")
 
 
+
+# CONVERSION EXAMPLES ---------------------------------------
+
+# weekly mileage for a week with 6 hours of running that averages 7:45 pace
+pace_to_run_miles(run_pace_mm_ss = "07:45",
+                  hrs = 6)
+
+# example of a deload week
+pace_to_run_miles(run_pace_mm_ss = "08:30",
+                  hrs = 4.5)
+
+
 # INTERVAL EXAMPLES ---------------------------------------
 
 # generate fixed-time intervals for swimming
 # 10 evenly-paced sets, leaving the wall every 1:55
 fixed_time_intervals(10, "02:15")
+
+# with target return times
+fixed_time_intervals(10,
+                     leave_every_mm_ss = "02:15",
+                     target_work_times = "01:48" )
 
 # fancier 2-wave ladder structure with 4th set easier
 fixed_time_intervals(8,
--- helper_workout.R
+++ helper_workout.R
@@ -1,5 +1,25 @@
 
 
+# FNS FOR PACE CONVERSIONS ---------------------------------------------
+
+
+### Convert running hours into miles. Especially useful for converting cross-training hours using running paces equivalent to the intensity of the cross-training (e.g., based on time trial results).
+
+# run_pace_mm_ss: quoted string "mm:ss" for average run pace (e.g., over a week) or cross-training equivalent
+# hrs: numeric hours of training
+
+pace_to_run_miles = function( run_pace_mm_ss,
+                              hrs
+) {
+  require(lubridate)
+  
+  sec_per_mile = period_to_seconds( ms(run_pace_mm_ss) )
+  
+  (hrs*(60^2))/sec_per_mile
+  
+}
+
+
 # FNS FOR INTERVALS ---------------------------------------------
 
 ### For fixed-time intervals (e.g., swim intervals where you leave the wall
@@ -20,13 +40,13 @@ fixed_time_intervals = function(n_sets,
   require(lubridate)
   
   leave_every_sec = period_to_seconds( ms(leave_every_mm_ss) )
-
+  
   
   # last time I should leave the wall
   leave_time_sec = ( 0 : (n_sets-1) ) * leave_every_sec 
   
   d = data.frame( set = 1:n_sets,
-              leave_at = sec_to_mm_ss(leave_time_sec) )
+                  leave_at = sec_to_mm_ss(leave_time_sec) )
   
   
   
@@ -41,7 +61,7 @@ fixed_time_intervals = function(n_sets,
     }
     
     target_work_sec = period_to_seconds( ms(target_work_times) )
-   
+    
     d$return_at = sec_to_mm_ss(leave_time_sec + target_work_sec)
   }
   
